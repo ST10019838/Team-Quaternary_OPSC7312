3cba27c717030add1026eb29c2c04cf327d5e3f7
DJDare <djdare2003@gmail.com>
2024-09-14 18:03:02 +0200

    fix: removed accidental api key and url exposure

--- bot-lobby-application/app/src/main/java/com/example/bot_lobby/api/RetrofitInstance.kt
+++ bot-lobby-application/app/src/main/java/com/example/bot_lobby/api/RetrofitInstance.kt
@@ -4,14 +4,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    val apiKey =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZud2Jiemd1aG5ra3JncnpucGR0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNTUyMjA1NywiZXhwIjoyMDQxMDk4MDU3fQ.n7oDojo_o1YPSmutTliymByx08KWoZLPA48uax2yQdM"
-
+    val apiKey = ""
 //    private const val BASE_URL =
 //        "https://android-kotlin-fun-mars-server.appspot.com"
 
     private const val BASE_URL =
-        "https://vnwbbzguhnkkrgrznpdt.supabase.co/rest/v1/"
+        ""
 
     val Api by lazy {
         Retrofit.Builder()
